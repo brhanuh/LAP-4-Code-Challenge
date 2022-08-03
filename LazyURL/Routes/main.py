8dc88df9a403c25328424295938f811f60f152c3
brhanuh <hanibalbrhanu15@hotmail.com>
2022-08-03 15:13:06 +0100

    removed short url

--- LazyURL/Routes/main.py
+++ LazyURL/Routes/main.py
@@ -13,10 +13,7 @@ def welcome():
         new_url = LazyUrl(original_url=long_url, short_url=short_url)
         db.session.add(new_url)
         db.session.commit()
-        short = f"https://fp-lazyurl.herokuapp.com/{short_url}/"
-        return render_template("index.html", short_url= short)
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
 @lazy_index.route("/<short>", methods=['GET'])
 def handle_url(short):
